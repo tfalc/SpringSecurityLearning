d84ff540c022900db67d0fb4146f5034b92be944
Thiago F <thiago.falcao86@gmail.com>
2022-11-15 21:23:26 -0300

    Create User.java

---
+++ src/main/java/tfalc/springsecurity/model/User.java
@@ -0,0 +1,40 @@
+package tfalc.springsecurity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "tab_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String username;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tab_user_roles", joinColumns = @JoinColumn(name = "user"))
+    @Column(name = "role_id")
+    private List<String> roles = new ArrayList<String>();
+
+    //getters and setters
+
+}
