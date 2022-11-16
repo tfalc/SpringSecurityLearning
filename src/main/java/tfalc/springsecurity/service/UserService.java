c7bbda3cf51fd89eabd2def016d799453427e294
Thiago F <thiago.falcao86@gmail.com>
2022-11-15 21:26:34 -0300

    Update UserService.java

--- src/main/java/tfalc/springsecurity/service/UserService.java
+++ src/main/java/tfalc/springsecurity/service/UserService.java
@@ -1,4 +1,23 @@
 package tfalc.springsecurity.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tfalc.springsecurity.model.User;
+import tfalc.springsecurity.repository.UserRepository;
+
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void createUser(User user) {
+        String pass = user.getPassword();
+
+        //criptografa antes de salvar em banco
+        user.setPassword(passwordEncoder.encode(pass));
+        userRepository.save(user);
+    }
 }
